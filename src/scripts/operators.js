b580fe61ca74a7b89bcdabb4358414c2e1b19215
smartikweb <smartikweb@gmail.com>
2019-12-08 23:13:19 +0200

    Added Task3 Operators

--- src/scripts/operators.js
+++ src/scripts/operators.js
@@ -1,10 +1,5 @@
-import "../styles/index.scss";
 const age = 17;
-if (age >= 18 && age <= 21) {
-  console.log("Your age is 18-21 years");
-} else {
-  console.log("Your age is not 18-21 years");
-}
+
 // 1) Проверьте переменную age и если возраст больше или равен 18 и меньше или равен 21, выведите в консоль соответсвующее сообщение
 
 const users = [
@@ -33,25 +28,14 @@ const users = [
     boughtItems: true
   }
 ];
-const filtered2Users = users.filter(
-  user => user.visitedPark == true && user.boughtItems == true
-);
 
-if (filtered2Users.length == 0) {
-  console.log("There are no users");
-} else {
-  console.table(filtered2Users);
-}
 // 2) Для каждого элемента массива users выведите сообщение в консоль, только если пользователь посетил парк или совершил покупку
 // Используйте foreach или for, а потом if внутри петли
 
 let iterationCounter = "Each iteration adds a *: ";
 
 for (let i = 0; i < 10; i++) {
-  iterationCounter += "*";
-  let i = "*";
+  console.log(i);
+  // 3) Используя assigment operator(+=) поменяйте значение переменоой iterationCounter, добавляя по одной звездочке при каждой итерации
 }
-console.log(iterationCounter);
-// 3) Используя assigment operator(+=) поменяйте значение переменоой iterationCounter, добавляя по одной звездочке при каждой итерации
-
 // 4) Выведите в консоль значение iterationCounter. Ожидаемый результат "Each iteration adds a *: * * * * * * * * * *"
